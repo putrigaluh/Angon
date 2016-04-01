3d427c17b7c1477efc8d673e0a1d485302273696
paramanisa <intan.paramanisa@gmail.com>
2016-04-01 12:20:30 +0700

    Revert "update 30 maret"
    This reverts commit e78d7f8e8102e5e91d450c4c56c6ccc802905026.

--- application/models/transaksi_model.php
+++ application/models/transaksi_model.php
@@ -1,15 +1,10 @@
 <?php
-
 class Transaksi_Model extends CI_Model {
     var $table = 'transaksi';
-    var $field = 'status';
-    public $id_trans;
-    public $nama_user;
-    public $id_user;
-    public $alamat_pengiriman;    
-    public $id_kota;
-    public $no_telp;
+    var $details;
+
     
+
     
     public function showTransaksi(){
          
@@ -17,34 +12,5 @@ class Transaksi_Model extends CI_Model {
          from transaksi t, user u where t.id_user = u.id_user ORDER BY id_transaksi DESC ";
         return $this->db->query($sql)->result();
     }
-
-    function update($id, $status) {
-        $data = array (
-            'status' => $status,
-        );
-        $this->db->where('id_transaksi', $id);
-        $this->db->update('transaksi', $data);
-    }
-    public function insert(){
-    
-    $tgl=date('Y-m-d');
-    //$status ='Pending';
-    $data = array(
-
-        'id_transaksi' => $this->input->post('id_transaksi'),
-        'tgl_transaksi' => $this->input->post('tgl'),
-        'id_user' => $this->input->post('id_user'),
-        'alamat_pengiriman' => $this->input->post('alamat_pengiriman'),
-        'id_kota' => $this->input->post('kota'),
-        'no_telp' => $this->input->post('no_telp'),
-        'status' => $this->input->post('status'),
-        );
-    $insert = $this->db->insert('transaksi', $data); 
-    return $insert;
-
-    
-    }
-   
 }
-
 ?>
--- application/views/manage/lihat_produk.php
+++ application/views/manage/lihat_produk.php
@@ -575,74 +575,105 @@ License: You must have a valid license purchased only from themeforest(the above
 								<table class="table table-striped table-bordered table-hover" id="datatable_products">
 								<thead>
 								<tr role="row" class="heading">
-									<th width="10%">
-										 ID Kategori Jenis
-									</th>
-									<th width="15%">
-										 Nama&nbsp;Produk
-									</th>
-									<th width="15%">
-										 Harga&nbsp;Produk
-									</th>
-									<th width="15%">
-										 Stok
-									</th>
-									<th width="15%">
-										 Deskripsi
-									</th>
-									<th width="15%">
-										 ID&nbsp;User
+									<th width="1%">
+										<input type="checkbox" class="group-checkable">
 									</th>
 									<th width="10%">
-										 Aksi
+										 ID
+									</th>
+									<th width="15%">
+										 Product&nbsp;Name
+									</th>
+									<th width="15%">
+										 Category
+									</th>
+									<th width="10%">
+										 Price
+									</th>
+									<th width="10%">
+										 Quantity
+									</th>
+									<th width="15%">
+										 Date&nbsp;Created
+									</th>
+									<th width="10%">
+										 Status
+									</th>
+									<th width="10%">
+										 Actions
 									</th>
 								</tr>
-								<?php
-									foreach ($p as $produk) {
-									?>
 								<tr role="row" class="filter">
 									<td>
-										<input type="text" class="form-control form-filter input-sm" 
-										value="<?php print $produk->id_kategori_jenis; ?>" 
-										name="id_kategori_jenis" readonly >
 									</td>
 									<td>
-										<input type="text" class="form-control form-filter input-sm" 
-										value="<?php print $produk->nama_produk; ?>" 
-										name="nama_produk" readonly >
+										<input type="text" class="form-control form-filter input-sm" name="product_id">
 									</td>
 									<td>
-										<input type="text" class="form-control form-filter input-sm" 
-										value="<?php print $produk->harga_produk; ?>" 
-										name="harga_produk" readonly >
+										<input type="text" class="form-control form-filter input-sm" name="product_name">
 									</td>
 									<td>
-										<input type="text" class="form-control form-filter input-sm" 
-										value="<?php print $produk->stok; ?>" 
-										name="stok" readonly >
-									</td>
-									<td>
-										<input type="text" class="form-control form-filter input-sm" 
-										value="<?php print $produk->deskripsi; ?>" 
-										name="deskripsi" readonly >
-									</td>
-									<td>
-										<input type="text" class="form-control form-filter input-sm" 
-										value="<?php print $produk->id_user; ?>" 
-										name="id_user" readonly >
+										<select name="product_category" class="form-control form-filter input-sm">
+											<option value="">Select...</option>
+											<option value="1">Mens</option>
+											<option value="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Footwear</option>
+											<option value="3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Clothing</option>
+											<option value="4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Accessories</option>
+											<option value="5">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fashion Outlet</option>
+											<option value="6">Football Shirts</option>
+											<option value="7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Premier League</option>
+											<option value="8">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Football League</option>
+											<option value="9">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Serie A</option>
+											<option value="10">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bundesliga</option>
+											<option value="11">Brands</option>
+											<option value="12">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Adidas</option>
+											<option value="13">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nike</option>
+											<option value="14">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Airwalk</option>
+											<option value="15">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;USA Pro</option>
+											<option value="16">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kangol</option>
+										</select>
 									</td>
 									<td>
 										<div class="margin-bottom-5">
-											<?php print "<a href=" . $this->config->base_url() . 'manage/produk/edit/' . 
-											$produk->id_produk . ">edit</a>" ?>
+											<input type="text" class="form-control form-filter input-sm" name="product_price_from" placeholder="From"/>
 										</div>
+										<input type="text" class="form-control form-filter input-sm" name="product_price_to" placeholder="To"/>
+									</td>
+									<td>
 										<div class="margin-bottom-5">
-											<?php print "<a href=" . $this->config->base_url() . 'manage/produk/delete/' . 
-											$produk->id_produk . ">hapus</a>" ?>
+											<input type="text" class="form-control form-filter input-sm" name="product_quantity_from" placeholder="From"/>
 										</div>
+										<input type="text" class="form-control form-filter input-sm" name="product_quantity_to" placeholder="To"/>
+									</td>
+									<td>
+										<div class="input-group date date-picker margin-bottom-5" data-date-format="dd/mm/yyyy">
+											<input type="text" class="form-control form-filter input-sm" readonly name="product_created_from" placeholder="From">
+											<span class="input-group-btn">
+											<button class="btn btn-sm default" type="button"><i class="fa fa-calendar"></i></button>
+											</span>
+										</div>
+										<div class="input-group date date-picker" data-date-format="dd/mm/yyyy">
+											<input type="text" class="form-control form-filter input-sm" readonly name="product_created_to " placeholder="To">
+											<span class="input-group-btn">
+											<button class="btn btn-sm default" type="button"><i class="fa fa-calendar"></i></button>
+											</span>
+										</div>
+									</td>
+									<td>
+										<select name="product_status" class="form-control form-filter input-sm">
+											<option value="">Select...</option>
+											<option value="published">Published</option>
+											<option value="notpublished">Not Published</option>
+											<option value="deleted">Deleted</option>
+										</select>
+									</td>
+									<td>
+										<div class="margin-bottom-5">
+											<button class="btn btn-sm yellow filter-submit margin-bottom"><i class="fa fa-search"></i> Search</button>
+										</div>
+										<button class="btn btn-sm red filter-cancel"><i class="fa fa-times"></i> Reset</button>
 									</td>
 								</tr>
-								<?php } ?>
 								</thead>
 								<tbody>
 								</tbody>
